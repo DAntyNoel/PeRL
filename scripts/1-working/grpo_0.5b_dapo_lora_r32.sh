dc9efba004ffb33aa26c1a918634fff4d649175c
DAntyNoel-LUMIA <dantynoel@sjtu.edu.cn>
2026-01-11 14:17:10 +0800

    add slurms

---
+++ scripts/1-working/grpo_0.5b_dapo_lora_r32.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#SBATCH --job-name=grpo-0.5b-dapo-lora-r32
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:4
+#SBATCH --output=/home/fhshao/PeRL/outputs/logs/%x.%jo
+#SBATCH --error=/home/fhshao/PeRL/outputs/logs/%x.%je
+#SBATCH --partition=RTX4090,A100,ADA6000,L40S
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate perl
+unset ROCR_VISIBLE_DEVICES
+set -xeuo pipefail
+cd /home/fhshao/PeRL/
+
+
+unset WANDB_DISABLED
+export PATH=/data1/public/cuda/cuda-12.8/bin:$PATH
+export LD_LIBRARY_PATH=/data1/public/cuda/cuda-12.8/lib64/:$LD_LIBRARY_PATH
+OUTPUT_DIR=outputs/grpo_lora_qwen2.5_0.5b_$(date +%Y%m%d_%H%M%S)
+OUTPUT_DIR=outputs/debug
+LOG_FILE=${OUTPUT_DIR}/output.log
+
+mkdir -p ${OUTPUT_DIR}
+
+CUDA_VISIBLE_DEVICES=$(echo $SLURM_JOB_GPUS) ACCELERATE_LOG_LEVEL=info \
+    accelerate launch \
+    --main_process_port 29501 \
+    --config_file scripts/trl/accelerate/ds_zero2_4gpu.yaml \
+    run.py train \
+    --config.common.seed 42 \
+    --config.common.debug false \
+    --config.model.model_name_or_path "/home/fhshao/downloads/models/Qwen/Qwen2.5-0.5B" \
+    --config.model.dtype "bfloat16" \
+    --config.peft.use_peft true \
+    --config.peft.type "lora" \
+    --config.peft.task_type "CAUSAL_LM" \
+    --config.peft.r 16 \
+    --config.peft.lora_alpha 32 \
+    --config.peft.lora_dropout 0.05 \
+    --config.peft.total_step 1000 \
+    --config.peft.target_modules '["q_proj","v_proj","k_proj","o_proj","up_proj","down_proj","gate_proj"]' \
+    --config.training.learning_rate 1e-5 \
+    --config.training.beta 0.0 \
+    --config.training.output_dir "${OUTPUT_DIR}" \
+    --config.training.run_name "${OUTPUT_DIR}" \
+    --config.training.remove_unused_columns false \
+    --config.training.gradient_accumulation_steps 8 \
+    --config.training.num_train_epochs 1 \
+    --config.training.max_completion_length 8192   \
+    --config.training.num_generations 8 \
+    --config.training.warmup_ratio 0.0 \
+    --config.training.max_prompt_length 512 \
+    --config.training.logging_steps 1 \
+    --config.training.per_device_train_batch_size 1 \
+    --config.training.save_strategy "steps" \
+    --config.training.save_steps 64 \
+    --config.training.max_steps 1024 \
+    --config.training.use_vllm true \
+    --config.training.top_entropy_quantile 1.0 \
+    --config.training.epsilon_high 0.28 \
+    --config.training.lr_scheduler_type "constant" \
+    --config.training.lr_scheduler_kwargs.min_lr_rate 0.1 \
+    --config.training.vllm_mode "colocate" \
+    --config.training.vllm_gpu_memory_utilization 0.4 \
+    --config.training.use_liger_kernel false \
+    --config.training.loss_type "dapo" \
+    --config.training.report_to '["tensorboard"]' \
+    --config.dataset.dataset_name_or_path "/home/fhshao/PeRL/downloads/datasets/openr1/DAPO-Math-17k" \
+    --config.dataset.example_numbers 1000000000 \
+    &> ${LOG_FILE}
